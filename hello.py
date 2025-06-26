f2113aaba2af866ae8719c1f77c6148e2812c080
TatyBR <taytaramos@hotmail.com>
2025-06-26 00:55:02 +0000

    arquivo hello.py criado

---
+++ hello.py
@@ -0,0 +1,2 @@
+print("Hello, EBAC! Este é um exercício prático de como criar um projeto remoto no GitHub,")
+print("Cloná-lo para nosso máquina local, e após modificações, atualizá-lo também remotamente!")
