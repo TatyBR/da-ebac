727496f8c048187ede24bbf8afce57f81942047b
TatyBR <taytaramos@hotmail.com>
2025-06-30 23:53:10 +0000

    arquivo hello.py alterado na nova branch dev

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello, EBAC! Este é um exercício prático de como criar um projeto remoto no GitHub,")
 print("Cloná-lo para nosso máquina local, e após modificações, atualizá-lo também remotamente!")
 print("Agora estamos no módulo 18, testando novos comandos do git!!")
+print("Estamos no mód 19: criando uma nova branch que conterá este arquivo hello!!!")
